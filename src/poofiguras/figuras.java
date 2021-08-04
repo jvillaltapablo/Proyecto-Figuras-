958584fb593f81a303331eb3d9f71b7d632a41ed
unknown <josepvillalta@outlook.com>
2021-08-03 19:20:08 -0600

    Proyecto figuras

---
+++ src/poofiguras/figuras.java
@@ -0,0 +1,21 @@
+package poofiguras;
+
+import javax.swing.JOptionPane;
+
+public class figuras {
+
+    public void cuadrado() {
+        int lado, area;
+
+        lado = Integer.parseInt(JOptionPane.showInputDialog("Digite el lado del cuadrado"));
+        area = lado * lado;
+        JOptionPane.showMessageDialog(null, "El area del cuadrado es :" + area);
+    }
+    public void triangulo() {
+        double base, altura, area;
+        base = Double.parseDouble(JOptionPane.showInputDialog("Digite la base del triangulo"));
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Digite la altura del triangulo"));
+        area=(base*altura)/2;
+        JOptionPane.showMessageDialog(null, "El area del triangulo es :" + area);
+    }
+}
---
+++ src/poofiguras/principal.java
@@ -0,0 +1,11 @@
+
+package poofiguras;
+
+public class principal {
+    public static void main(String[] args){
+        figuras datos = new figuras();
+        datos.cuadrado();
+        datos.triangulo();
+    }
+    
+}
